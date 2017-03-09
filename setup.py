05c299cbd0030d5c9e4ff4b22e551d9f04e74b39
Julia Willson <julia.willson@gmail.com>
2017-03-09 09:14:39 -0800

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="data-structures",
+    version="1.0.0",
+    long_description=__doc__,
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+)
